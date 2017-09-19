aba4bf2b3a21c744511b3534cd847ea40787fc80
wushj <wushj@yonyou.com>
2017-09-19 17:20:15 +0800

    循环队列

--- src/dataStructure/queue/MyQueue.java
+++ src/dataStructure/queue/MyQueue.java
@@ -20,15 +20,33 @@ public class MyQueue {
         end = -1;
     }
 
+    /**
+     * 循环插入
+     * @param value
+     */
     public void insert(long value){
+        //如果end移到最尾则返回头部
+        if(end == maxSize){
+            end = -1;
+        }
         end++;
         arr[end] = value;
         size++;
     }
 
+    /**
+     * 循环删除
+     * @return
+     */
     public long remove(){
         long value = arr[front];
         front++;
+
+        //如果front移到最大 循环放到最开始
+        if(front == maxSize){
+            front = 0;
+        }
+
         size--;
         return value;
     }
